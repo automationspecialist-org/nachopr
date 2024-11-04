7fa448ef39c0fb6cf1da9388dd4c3de92a273e3a
Duncan Felix <d@uncan.net>
2024-11-04 15:13:38 +0100

    x

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from core.models import NewsSource, NewsPage, Journalist
 
-# Create your views here.
+def home(request):
+    news_sources_count = NewsSource.objects.count()
+    news_pages_count = NewsPage.objects.count()
+    journalist_count = Journalist.objects.count()
+    context = {
+        'news_sources_count': news_sources_count,
+        'news_pages_count': news_pages_count,
+        'journalist_count': journalist_count
+    }
+    return render(request, 'core/home.html', context=context)
