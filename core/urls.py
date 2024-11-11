a0b151caa6859948e23239cc44b7092b90b9d735
Duncan Felix <d@uncan.net>
2024-11-11 11:45:00 +0100

    x

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
     path('search-results/', views.search_results, name='search_results'),
-    path('free-media-list/', views.free_media_list, name='free-media-list'),
+    path('free-media-database/', views.free_media_list, name='free-media-list'),
     path('signup/', views.signup, name='signup'),
 ]
 
