10912955a6bf599e3ec514486e9211225e6b8a98
Duncan Felix <d@uncan.net>
2024-11-11 15:41:19 +0100

    x

--- core/cron.py
+++ core/cron.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 import requests
 import os
 
-load_dotenv()
+if 'AZURE' not in os.environ:
+    load_dotenv()
 
 logger = logging.getLogger(__name__)
 
 def test_job():
-    slack_webhook_url = "https://hooks.slack.com/services/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"
-    message = f"[{timezone.now()}] NachoPR Test job executed."
+    slack_webhook_url = "https://hooks.slack.com/services/T07M0BTS3A8/B07MLUL3QKS/eE3D3EK7BshbSS1feJ345hK"
+    message = f"[{timezone.now()}] NachoPR Test job executed!"
     logger.info(message)
     requests.post(slack_webhook_url, json={"text": message})
