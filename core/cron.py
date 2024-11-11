ffd35c2e3b351e41f80dfd8c459090ffbbc7e790
Duncan Felix <d@uncan.net>
2024-11-11 16:38:52 +0100

    x

--- core/cron.py
+++ core/cron.py
@@ -10,8 +10,8 @@ if 'AZURE' not in os.environ:
 logger = logging.getLogger(__name__)
 
 def test_job():
-    slack_webhook_url = "https://hooks.slack.com/services/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"
-    message = f"[{timezone.now()}] NachoPR Test job executed!"
+    slack_webhook_url = os.getenv("SLACK_WEBHOOK_URL")
+    message = f"[{timezone.now()}] NachoPR Test job executed! (used env var)"
     logger.info(message)
     response = requests.post(slack_webhook_url, json={"text": message})
     logger.info(f"Slack response: {response.status_code} {response.text}")
