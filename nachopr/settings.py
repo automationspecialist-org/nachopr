e9d40faf59444ad9f864e7a086bd6b87d9b264a3
Duncan Felix <d@uncan.net>
2024-11-18 08:15:52 +0100

    x

--- nachopr/settings.py
+++ nachopr/settings.py
@@ -217,6 +217,7 @@ CRONJOBS = [
     ('*/30 * * * *', 'core.cron.categorize_job', '>> /tmp/cron_categorize.log 2>&1'),
     ('*/30 * * * *', 'core.cron.process_journalist_profiles_job', '>> /tmp/cron_process_journalist_profiles.log 2>&1'),
     ('*/30 * * * *', 'core.cron.guess_emails_job', '>> /tmp/cron_guess_emails.log 2>&1'),
+    ('*/30 * * * *', 'core.cron.clean_db_job', '>> /tmp/cron_clean_db.log 2>&1'),
 ]
 
 DJSTRIPE_FOREIGN_KEY_TO_FIELD = os.environ.get('DJSTRIPE_FOREIGN_KEY_TO_FIELD', 'djstripe_id')
