e08180bec0d69ab1c167732e960fd04a96e717f3
Duncan Felix <d@uncan.net>
2024-11-24 21:24:45 +0100

    x

--- core/celery.py
+++ core/celery.py
@@ -2,14 +2,19 @@ import os
 from celery import Celery
 from dotenv import load_dotenv
 from celery.signals import celeryd_after_setup
+import logging
 
 load_dotenv()
+
+# Configure Celery logging
+logger = logging.getLogger('celery')
+logger.setLevel(logging.DEBUG)
 
 sas_policy_name = os.getenv('AZURE_QUEUE_POLICY_NAME')
 sas_key = os.getenv('AZURE_QUEUE_POLICY_KEY')
 namespace = os.getenv('AZURE_QUEUE_HOST')
 
-# Set the default Django settings module
+# Set the default Django settings module BEFORE any Django imports
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nachopr.settings')
 
 # Create the Celery app
@@ -23,7 +28,8 @@ app = Celery(
         "uamqp_keep_alive_interval": 30,
         "retry_total": 3,
         "retry_backoff_factor": 0.8,
-        "retry_backoff_max": 120
+        "retry_backoff_max": 120,
+        "debug": True
     }
 )
 
@@ -36,9 +42,6 @@ app.autodiscover_tasks()
 @celeryd_after_setup.connect
 def setup_periodic_tasks(sender, instance, **kwargs):
     """Start continuous tasks when Celery worker starts"""
-    from core.tasks import (
-        continuous_crawl_task,
-    )
-    
-    # Start continuous tasks
+    # Import tasks here to avoid circular imports
+    from core.tasks import continuous_crawl_task
     continuous_crawl_task.delay()
