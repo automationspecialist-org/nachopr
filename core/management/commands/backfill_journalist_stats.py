06b8d1d5f66ccb721626cc5b81ac71347557cadf
Duncan Felix <d@uncan.net>
2024-12-02 12:41:41 +0100

    x

--- core/management/commands/backfill_journalist_stats.py
+++ core/management/commands/backfill_journalist_stats.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from datetime import timedelta
 from core.models import Journalist, DbStat
-from django.db.models import Q
 from django.db import transaction
 
 class Command(BaseCommand):
